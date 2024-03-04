5215f434d1605d89b3e12a4562193edcc31a159b
Thirumalai24 <162083328+Thirumalai24@users.noreply.github.com>
2024-03-04 13:08:30 +0530

    Add files via upload

---
+++ One.java
@@ -0,0 +1,18 @@
+package com.thapovan.developer;
+
+//if-else
+public class One {
+	
+	public static void main(String[] args) {
+				        int number = 30;
+		        // Check if the number is less than 0
+		        if (number > 10) {
+		            System.out.println("The number is greater.");
+		        }else
+		        System.out.println("The number is lesser");
+		    }
+		
+
+	}
+
+
